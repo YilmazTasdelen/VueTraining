566998137399b1054916beff4d10e43e9e016b78
Yilmaz <yilmaztasdelen1994@gmail.com>
2023-01-28 01:46:22 +0300

    .

--- starter/app.js
+++ starter/app.js
@@ -2,15 +2,28 @@ const viewModel = Vue.createApp({
     data() {
         return {
             firstName: 'John',
+            middleName: '',
             lastName: 'Doe',
             url: 'https://google.com',
-            raw_url: '<a href="https://google.com" target="_blank">Google</a>'
+            raw_url: '<a href="https://google.com" target="_blank">Google</a>',
+            age: 20
         }
     },
     methods: {
         // not recommented arrow functions couse this keyword not work 
         fullName() {
-            return `${this.firstName} ${this.lastName.toUpperCase()}`
+            return `${this.firstName} ${this.middleName} ${this.lastName.toUpperCase()}`
+        },
+        increment() {
+            this.age++
+        },
+        updateLastName(msg, event) {
+            // event.preventDefault()
+            console.log(msg)
+            this.lastName = event.target.value
+        },
+        updateMiddleName(event) {
+            this.middleName = event.target.value
         }
     }
 }).mount('#app')
